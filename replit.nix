8739b2cd85920d19c9e7f10f49734b185c9bac45
neoxr <neoxr@null.net>
2022-08-27 21:50:35 +0700

    +

--- replit.nix
+++ replit.nix
@@ -3,8 +3,8 @@
 	pkgs.nodejs-16_x
     pkgs.nodePackages.typescript-language-server
     pkgs.ffmpeg
+    pkgs.libwebp
     pkgs.nodePackages.yarn
     pkgs.replitPackages.jest
-    pkgs.git
 	];
 }
